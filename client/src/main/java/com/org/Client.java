be54fdb725c73ad991319c6f2c2b28ea3d636260
zhudecheng <862860351@qq.com>
2020-10-20 18:25:20 +0800

    cloud

--- client/src/main/java/com/org/Client.java
+++ client/src/main/java/com/org/Client.java
@@ -3,11 +3,12 @@ package com.org;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
-@EnableFeignClients
+@EnableDiscoveryClient //重点
+@EnableFeignClients("com.org.clinet")
 public class Client {
   public static void main(String[] args) {
 	  SpringApplication.run(Client.class,args);
